2319ae692e1fdc3a803d4fba75d96e33abbb3e89
Shubham Kumar <75445869+shubhamchauhan38@users.noreply.github.com>
2026-03-13 01:26:57 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+USE master;
+GO
+
+--CREATE THE 'DATAWAREHOUSE' DATABASE
+create database DataWarehouse;
+GO			
+
+use DataWarehouse;
+GO
+
+-- CREATE SCHEMA
+Create Schema bronze;
+GO
+Create Schema silver;
+Go
+Create Schema gold;
+Go
